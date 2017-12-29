0c343727215748ee622e991f21dec4feed8eadd0
Morgan Fudge <mfudge92@gmail.com>
2017-12-29 04:51:10 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+ 
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+ 
+      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|i| i.name == item_name}
+ 
+      resp.write song.artist
+    end
+ 
+    resp.finish
+  end
+end
